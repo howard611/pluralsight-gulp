4306d0f4ab2351a7f2673a57e286e7decf69509f
Howard <howard.tam.95@gmail.com>
2016-02-17 22:41:10 +0000

    Setup basic gulping

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var args = require('yargs').argv;
+var config = require('./gulp.config')();
+var $ = require('gulp-load-plugins')({lazy: 'true'});
+
+gulp.task('vet', function() {
+	log('Analyzing source with JSHint and JSCS');
+
+	return gulp
+			.src(config.alljs)
+			.pipe($.if(args.verbose, $.print()))
+			.pipe($.jscs())
+			.pipe($.jshint())
+			.pipe($.jshint.reporter('jshint-stylish', { verbose: true }))
+			.pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('style', function() {
+	log('Compiling Less --> CSS');
+
+	return gulp
+			.src(config.less)
+			.pipe($.if(args.verbose, $.print()))
+			.pipe($.less())
+			.pipe($.autoprefixer({ browsers: ['last 2 version', '> 5%']}))
+			.pipe(gulp.dest(config.temp));
+});
+
+///////////////
+
+function log(msg) {
+	if (typeof(msg) === 'object') {
+		for (var item in msg) {
+			if (msg.hasOwnProperty(item)) {
+				$.util.log($.util.colors.blue(msg[item]));
+			}
+		}
+	} else {
+		$.util.log($.util.colors.blue(msg));
+	}
+}
